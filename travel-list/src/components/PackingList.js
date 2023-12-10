7f525196b931e07b142d522a9e11747c65030b89
Stanislav Angelov <stannyweb@gmail.com>
2023-12-10 03:05:44 +0200

    Added Sorting

--- travel-list/src/components/PackingList.js
+++ travel-list/src/components/PackingList.js
@@ -1,13 +1,35 @@
+import { useState } from 'react'
+
 import Item from './Item'
 
 export default function PackingList ({ items }) {
+  const [sortBy, setSortBy] = useState('input')
+
+  let sortedItems
+
+  if (sortBy === 'input') sortedItems = items
+  if (sortBy === 'description')
+    sortedItems = [...items].sort((a, b) =>
+      //LocalCompare if we have strings to compare
+      a.description.localeCompare(b.description)
+    )
+  if (sortBy === 'packed')
+    sortedItems = [...items].sort((a, b) => +a.packed - +b.packed)
+
   return (
     <div className='list'>
       <ul>
-        {items.map(item => (
+        {sortedItems.map(item => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
+      <div className='actions'>
+        <select value={sortBy} onChange={e => setSortBy(e.target.value)}>
+          <option value='input'>Sort By input status</option>
+          <option value='description'>Sort By description</option>
+          <option value='packed'>Sort by Packed status</option>
+        </select>
+      </div>
     </div>
   )
 }
